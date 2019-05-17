00fccbd5edf9a8532b725dfec1c594c8f1e17dc3
veekxt <veekxt@gmail.com>
2019-05-18 05:30:44 +0800

    add character directory

--- rename_resources.py
+++ rename_resources.py
@@ -28,20 +28,3 @@ for dir_char in dir_voice:
                 newname +="_2.wav";
                 os.rename(os.path.join(path, dir_char, one_voice), os.path.join(path, "out", new_dir_char,newname))
             print(newname + " is ok")
-
-import os
-
-
-def mkdir(path):
-    folder = os.path.exists(path)
-
-    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-        print
-        "---  new folder...  ---"
-        print
-        "---  OK  ---"
-
-    else:
-        print
-        "---  There is this folder!  ---"
